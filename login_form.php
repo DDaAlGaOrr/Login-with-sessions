b8ea3a68447b58234f152687edaab4a6d8a779ec
daalgaor.17@gmail.com <daalgaor.17@gmail.com>
2020-08-09 18:19:11 -0600

    Creacion del formulario para iniciar sesion

--- login_form.php
+++ login_form.php
@@ -4,14 +4,38 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@700&family=PT+Sans&display=swap" rel="stylesheet">
+    
     <title>Login</title>
   </head>
-  <body>
-    <h1>Inicia sesion</h1>
+  <body style="background-color: #f3f3f4;">
+    <header>
+
+    </header>
+
+    <div class="container">
+      <form id="formulario" class="mx-auto w-50 p-5  rounded border my-5" style="background-color: white;"  name="formulario">
+      <h2 class="text-center mb-1" >Bienvenido inicia sesion</h2>
+      <h3 class="text-center">o</h3>
+      <h2 class="text-center mb-5" ><a class="aaa" href="">Crea una cuenta</a></h2>
+      <hr>
+            <div class="form-group">
+              <label for="">Correo electronico:</label>
+              <input class="form-control" name="email" id="email" type="email" placeholder="">
+            </div>
+            <div class="form-group">
+              <label for="">Contraseña:</label>
+              <input class="form-control" name="password" id="password" type="password" placeholder="">
+            </div>
+            <div class="text-center">
+              <button type="submit" class="btn btn-info mt-4" style="width: 100px;">Entrar</button>
+            </div>
+      </form>
+    </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
